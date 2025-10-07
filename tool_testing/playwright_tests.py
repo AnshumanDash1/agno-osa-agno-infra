0b54026cba80e82436d9cbcec84c2151e56ab49e
AnshumanDash1 <105815299+AnshumanDash1@users.noreply.github.com>
2025-10-06 20:47:22 -0400

    Test: Specific video click
    Made it so that a specific video can be clicked on by its title name!
    
    It works through using page.get_by_text instead of get by role. Never would have thought of it if I hadn't read the docs!

--- tool_testing/playwright_tests.py
+++ tool_testing/playwright_tests.py
@@ -1,24 +1,26 @@
 import asyncio
-import re
+import json
 from playwright.async_api import Playwright, async_playwright, expect
 
 CDP_URL = "http://localhost:9222"
-
 async def run(playwright: Playwright) -> None:
-    browser = await p.chromium.connect_over_cdp(CDP_URL)
-    context = browser.contexts[0]
-    page = context.pages[0]
-    await page.goto("https://www.youtube.com/")
-    await page.get_by_role("combobox", name="Search").click()
-    await page.get_by_role("combobox", name="Search").fill("jaws screen reader tutorial")
-    await page.get_by_role("button", name="Search", exact=True).click()
-    await page.locator("#inline-preview-player video").click()
-    await page.get_by_role("button", name="Skip", exact=True).click()
-    await page.get_by_role("button", name="Pause keyboard shortcut k").click()
+    async with async_playwright() as p:
+        browser = await p.chromium.connect_over_cdp(CDP_URL)
+        context = browser.contexts[0]
+        page = context.pages[0]
+        await page.goto("https://www.youtube.com/")
+        await page.get_by_role("combobox", name="Search").click()
+        await page.get_by_role("combobox", name="Search").fill("jaws screen reader tutorial")
+        await page.get_by_role("button", name="Search", exact=True).click()
+        await page.wait_for_load_state("networkidle")
+        snapshot = await page.locator("body").aria_snapshot()
+        print(snapshot)
+        await page.get_by_text("A brief introduction to JAWS").click()
+
 
     # ---------------------
-    await context.close()
-    await browser.close()
+        await context.close()
+        await browser.close()
 
 
 async def main() -> None:
